e20ad2a27720991d6ea41e54b176b3887cbbb507
slisaasquatch <sli@referralsaasquatch.com>
2016-10-27 09:04:05 -0700

    SL: changed the indentation to 2 spaces to match Ruby convention

--- recipes/install_geoip.rb
+++ recipes/install_geoip.rb
@@ -6,24 +6,24 @@ GEOIP_ABS = GEOIP_DIR + '/' + GEOIP_FN
 
 unless File.file?(GEOIP_ABS) then
     directory GEOIP_DIR do
-        action :create
-    end
+    action :create
+  end
 
-    execute 'download_geoip' do
-        command 'wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz'
-        cwd GEOIP_DIR
-    end
+  execute 'download_geoip' do
+    command 'wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz'
+    cwd GEOIP_DIR
+  end
 
-    execute 'unzip_geoip' do
-        command 'gzip -d ' + GEOIP_FN + '.gz'
-        cwd GEOIP_DIR
-    end
+  execute 'unzip_geoip' do
+    command 'gzip -d ' + GEOIP_FN + '.gz'
+    cwd GEOIP_DIR
+  end
 end
 
 log 'Done installing GeoIP'
 
 execute 'set_geoip_env' do
-    command 'export MAXMIND_DB_PATH=' + GEOIP_ABS
+  command 'export MAXMIND_DB_PATH=' + GEOIP_ABS
 end
 
 log 'Done setting GeoIP env variable'
