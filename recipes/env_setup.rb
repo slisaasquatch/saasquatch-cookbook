3d6b913d5c578def1d40fb699a9141a57a7ec4a6
slisaasquatch <sli@referralsaasquatch.com>
2016-10-28 12:11:12 -0700

    SL: simplified even more

--- recipes/env_setup.rb
+++ recipes/env_setup.rb
@@ -11,14 +11,6 @@ def bash_script_name
   '/home/vagrant/.bashrc'
 end
 
-def bash_script_contains(str)
-  file_contains(bash_script_name, str)
-end
-
-def bash_script_append(str)
-  file_append(bash_script_name, str)
-end
-
 def construct_export_line(key, val)
   key.strip!
   return '' if key.empty?
@@ -28,7 +20,7 @@ end
 
 def add_env(key, val)
   export_line = construct_export_line(key, val)
-  bash_script_append(export_line) unless bash_script_contains(export_line)
+  file_append(bash_script_name, export_line) unless file_contains(bash_script_name, export_line)
   execute 'set env ' + key do
     command export_line
   end
